52f9c3279749700adaf51ba3dcabfa928070c990
gervajac <gerva.jacob.tropini@gmail.com>
2023-10-09 18:50:40 -0300

    prueba

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv';
-config();
+console.log(process.env.PORT)
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT);
+  await app.listen(3008);
 }
 bootstrap();
