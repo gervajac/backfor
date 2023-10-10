fa3250aff2d5e8e43fa2e840a7a23c333eee462e
gervajac <gerva.jacob.tropini@gmail.com>
2023-10-09 23:26:58 -0300

    .env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-console.log(process.env.PORT)
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3008);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
